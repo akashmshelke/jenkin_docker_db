f123582265edc77206c10ade51338c7f340db609
Akash Shelke <akash.shelke@sunbeaminfo.com>
2019-11-22 06:10:33 +0530

    added more products

--- product.sql
+++ product.sql
@@ -1,3 +1,6 @@
-create table product(id int auto_increment primary key, title varchar(50), description varchar(150), price float(5,2));
+create table product(id int auto_increment primary key, title varchar(50), description varchar(150), price float(7,2));
 
-insert into product values(1,"Samsung","Mobile Phone",999.99);
+insert into product values(1,"Samsung","Mobile Phone",12000.99);
+insert into product values(2,"Mi TV","TV",12300.33);
+insert into product values(3,"HP","Laptop",99900.99);
+insert into product values(4,"Pendrive","SanDisk 256GB",10000.99);
